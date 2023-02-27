2555060ebbe48242f9cd5d0c759122fdf45bcfd0
José Luis Landabaso Díaz <landabaso@gmail.com>
2023-02-27 10:19:42 +0100

    Start ledger playground

--- descriptors/legacy2segwit/index.ts
+++ descriptors/legacy2segwit/index.ts
@@ -5,6 +5,7 @@ import { Psbt, networks } from 'bitcoinjs-lib';
 const { pkhBIP32, wpkhBIP32 } = descriptors.scriptExpressions;
 const { Descriptor, BIP32 } = descriptors.DescriptorsFactory(secp256k1);
 const network = networks.testnet;
+const EXPLORER = 'https://blockstream.info/testnet';
 const FEE = 500;
 
 //Let's create our software wallet with this mnemonic:
@@ -13,18 +14,18 @@ const MNEMONIC =
   'slice illness sniff distance carbon elder';
 const masterNode = BIP32.fromSeed(mnemonicToSeedSync(MNEMONIC), network);
 
-//Let's get the Legacy address where we know we have some initial money to play:
+//We started with 1679037 sats in mOVqJewPuO1miLpTHQiRgTKCLexL4MeAFVagts7HmNZ2Q
+//You can verify it in a block explorer: https://tinyurl.com/mu82nmzw
+const TXID = 'ee02b5a12c2f22e892bed376781fc9ed435f0d192a1b67ca47a7190804d8e868';
+
+//Let's calculate the Legacy address where we sent some initial money to play:
 const descriptorLegacy = new Descriptor({
   expression: pkhBIP32({ masterNode, network, account: 0, keyPath: '/0/1' }),
   network
 });
-//See, we start with 1679037 sats in moovc1JqGrz4v6FA2U8ks8ZqjSwjv3yRKQ:
-//https://tinyurl.com/mu82nmzw
-const TXID = 'ee02b5a12c2f22e892bed376781fc9ed435f0d192a1b67ca47a7190804d8e868';
 console.log(`We start with:`, { address: descriptorLegacy.getAddress(), TXID });
 
-//Let's get the utxo info (txHex and vout) of the Legacy address.
-const EXPLORER = 'https://blockstream.info/testnet';
+//Let's get the utxo info (txHex & vout) of the initial tx to the Legacy address
 (async () => {
   const txHex = await (await fetch(`${EXPLORER}/api/tx/${TXID}/hex`)).text();
   const txJson = (await (await fetch(`${EXPLORER}/api/tx/${TXID}`)).json()) as {
@@ -35,10 +36,10 @@ const EXPLORER = 'https://blockstream.info/testnet';
     txOut =>
       txOut.scriptpubkey === descriptorLegacy.getScriptPubKey().toString('hex')
   );
-  const initialValue = txOuts[vout]!.value; //1679037
+  const initialValue = txOuts[vout]!.value; //This must be: 1679037
   console.log('This is the utxo to spend :', { txHex, vout, initialValue });
 
-  //Define the Segwit descriptor where we will move the sats above:
+  //Define the Segwit descriptor where we will move the funds:
   const descriptorSegwit = new Descriptor({
     expression: wpkhBIP32({ masterNode, network, account: 0, keyPath: '/1/0' }),
     network
@@ -58,7 +59,7 @@ const EXPLORER = 'https://blockstream.info/testnet';
   descriptors.signers.signBIP32({ psbt, masterNode });
   descriptorLegacy.finalizePsbtInput({ psbt, index: legacyInputNumber });
   const spendTx = psbt.extractTransaction();
-  //It won't be accepted (again) when you try, indeed.
+  //When you try this, it won't be accepted (again), indeed.
   const spendTxPushResult = await (
     await fetch('https://blockstream.info/testnet/api/tx', {
       method: 'POST',
