339c41ba064ba86e6b1e93efaedd74be30c52e42
José Luis Landabaso Díaz <landabaso@gmail.com>
2023-03-23 12:14:38 +0100

    New miniscript playground (an absolute timelock vault with emergency exit path)

--- descriptors/miniscript/index.ts
+++ descriptors/miniscript/index.ts
@@ -4,13 +4,14 @@
 import * as secp256k1 from '@bitcoinerlab/secp256k1';
 import * as descriptors from '@bitcoinerlab/descriptors';
 import { compilePolicy } from '@bitcoinerlab/miniscript';
-import { /*Psbt,*/ networks } from 'bitcoinjs-lib';
-import { mnemonicToSeedSync } from 'bip39';
-// @ts-ignore
-import { encode as olderEncode } from 'bip68';
+import { Psbt, networks } from 'bitcoinjs-lib';
+import { generateMnemonic, mnemonicToSeedSync } from 'bip39';
 // @ts-ignore
 import { encode as afterEncode } from 'bip65';
-const { Descriptor, BIP32 } = descriptors.DescriptorsFactory(secp256k1);
+import { readFileSync, writeFileSync } from 'fs';
+import type { ECPairInterface } from 'ecpair';
+
+const { Descriptor, BIP32, ECPair } = descriptors.DescriptorsFactory(secp256k1);
 
 const network = networks.testnet;
 const EXPLORER = 'https://blockstream.info/testnet';
@@ -24,92 +25,107 @@ const Log = (message: string) => {
   console.log(message.replace(/<[^>]*>?/gm, '')); //strip html tags
 };
 
+//JSON to pretty-string format:
+const JSONf = (json: object) => JSON.stringify(json, null, '\t');
+
+const EMERGENCY_RECOVERY = false;
 const BLOCKS = 5;
-
-const POLICY = (older: number, after: number) =>
-  `or(and(pk(@olderKey),older(${older})),and(pk(@afterKey),after(${after})))`;
-
+const POLICY = (after: number) =>
+  `or(pk(@emergencyKey),and(pk(@unvaultKey),after(${after})))`;
 const WSH_ORIGIN_PATH = `/69420'/1'/0'`; //This can be any path you like.
 const WSH_KEY_PATH = `/0/0`; //Choose any path.
 
-const SOFT_MNEMONIC = `abandon abandon abandon abandon abandon abandon abandon \
-abandon abandon abandon abandon about`;
-const masterNode = BIP32.fromSeed(mnemonicToSeedSync(SOFT_MNEMONIC), network);
+Log(`This test data: ${JSONf({ EMERGENCY_RECOVERY, BLOCKS })}`);
+
+let emergencyPair: ECPairInterface;
+let unvaultMnemonic;
+if (isWeb) {
+  const emergencyWIF = localStorage.getItem('emergencyWIF');
+  unvaultMnemonic = localStorage.getItem('unvaultMnemonic');
+  if (!emergencyWIF || !unvaultMnemonic) {
+    emergencyPair = ECPair.makeRandom();
+    unvaultMnemonic = generateMnemonic();
+    localStorage.setItem('emergencyWIF', emergencyPair.toWIF());
+    localStorage.setItem('unvaultMnemonic', unvaultMnemonic);
+  } else emergencyPair = ECPair.fromWIF(emergencyWIF);
+} else {
+  try {
+    emergencyPair = ECPair.fromWIF(readFileSync('.emergencyWIF', 'utf8'));
+    unvaultMnemonic = readFileSync('.unvaultMnemonic', 'utf8');
+  } catch {
+    emergencyPair = ECPair.makeRandom();
+    unvaultMnemonic = generateMnemonic();
+    writeFileSync('.emergencyWIF', emergencyPair.toWIF());
+    writeFileSync('.unvaultMnemonic', unvaultMnemonic);
+  }
+}
+
+const unvaultMasterNode = BIP32.fromSeed(
+  mnemonicToSeedSync(unvaultMnemonic),
+  network
+);
+Log(
+  `Your secrets 🤫: ${JSONf({
+    emergencyWIF: emergencyPair.toWIF(),
+    unvaultMnemonic
+  })}`
+);
 
 const start = async () => {
   const currentBlockHeight = parseInt(
     await (await fetch(`${EXPLORER}/api/blocks/tip/height`)).text()
   );
-
-  Log(`${currentBlockHeight}`);
-
   const after = afterEncode({ blocks: currentBlockHeight + BLOCKS });
-  const older = olderEncode({ blocks: BLOCKS }); //relative locktime (sequence)
+  Log(`Policy: ${JSONf({ currentBlockHeight, policy: POLICY(after) })}`);
   //Now let's prepare the wsh utxo:
-  const { miniscript, issane } = compilePolicy(POLICY(older, after));
-  console.log({
-    Descriptor,
-    miniscript,
-    WSH_ORIGIN_PATH,
-    WSH_KEY_PATH,
-    masterNode
-  });
+  const { miniscript, issane } = compilePolicy(POLICY(after));
   if (!issane) throw new Error(`Error: miniscript not sane`);
-  /*
+  const unvaultKey = unvaultMasterNode.derivePath(
+    `m${WSH_ORIGIN_PATH}${WSH_KEY_PATH}`
+  ).publicKey;
   const wshExpression = `wsh(${miniscript
-    .replace('@ledger', ledgerKeyExpression)
-    .replace('@soft', softKeyExpression)})`;
+    .replace(
+      '@unvaultKey',
+      descriptors.keyExpressionBIP32({
+        masterNode: unvaultMasterNode,
+        originPath: WSH_ORIGIN_PATH,
+        keyPath: WSH_KEY_PATH
+      })
+    )
+    .replace('@emergencyKey', emergencyPair.publicKey.toString('hex'))})`;
   const wshDescriptor = new Descriptor({
     expression: wshExpression,
     network,
-    preimages: [{ digest: `sha256(${DIGEST})`, preimage: PREIMAGE }]
+    signersPubKeys: [EMERGENCY_RECOVERY ? emergencyPair.publicKey : unvaultKey]
   });
   const wshAddress = wshDescriptor.getAddress();
-
-  //Now, spend both wpkh and wsh utxos:
   const psbt = new Psbt({ network });
-  const psbtInputDescriptors = [];
-  Log(`Fund the utxos. Let's first check if they're already funded...`);
-  const wpkhUtxo = await (
-    await fetch(`${EXPLORER}/api/address/${wpkhAddress}/utxo`)
-  ).json();
-  const wshUtxo = await (
+  Log(`Fund your vault. Let's first check if it's been already funded...`);
+  const utxo = await (
     await fetch(`${EXPLORER}/api/address/${wshAddress}/utxo`)
   ).json();
-  if (wpkhUtxo?.[0] && wshUtxo?.[0]) {
-    Log(`Successfully funded. Now let's spend them. Go to your Ledger now! You \
-may need to register the Policy (only once) and then accept spending 2 utxos.`);
-    let txHex = await (
-      await fetch(`${EXPLORER}/api/tx/${wpkhUtxo?.[0].txid}/hex`)
+  if (utxo?.[0]) {
+    Log(`Successfully funded. Now let's spend the funds.`);
+    const txHex = await (
+      await fetch(`${EXPLORER}/api/tx/${utxo?.[0].txid}/hex`)
     ).text();
-    let inputValue = wpkhUtxo[0].value;
-    let i = wpkhDescriptor.updatePsbt({ psbt, txHex, vout: wpkhUtxo[0].vout });
-    psbtInputDescriptors[i] = wpkhDescriptor;
-    txHex = await (
-      await fetch(`${EXPLORER}/api/tx/${wshUtxo?.[0].txid}/hex`)
-    ).text();
-    inputValue += wshUtxo[0].value;
-    i = wshDescriptor.updatePsbt({ psbt, txHex, vout: wshUtxo[0].vout });
-    psbtInputDescriptors[i] = wshDescriptor;
-    //We'll send the funds to one of our Ledger's internal (change) addresses:
-    const finalAddress = new Descriptor({
-      expression: await descriptors.scriptExpressions.wpkhLedger({
-        ledgerClient,
-        ledgerState,
-        network,
-        account: 0,
-        change: 1,
-        index: 0
-      }),
-      network
-    }).getAddress();
+    const inputValue = utxo[0].value;
+    wshDescriptor.updatePsbt({ psbt, txHex, vout: utxo[0].vout });
+    //Send funds to any address: "abandon^11 about" mnemonic 1st/2nd pkh address
     //Be nice. Give the miners 1000 sats :)
-    psbt.addOutput({ address: finalAddress, value: inputValue - 1000 });
+    psbt.addOutput({
+      address: EMERGENCY_RECOVERY
+        ? 'mkpZhYtJu2r87Js3pDiWJDmPte2NRZ8bJV'
+        : 'mzpbWabUQm1w8ijuJnAof5eiSTep27deVH',
+      value: inputValue - 1000
+    });
 
     //Now sign the PSBT with the BIP32 node (the software wallet)
-    descriptors.signers.signBIP32({ psbt, masterNode });
+    if (EMERGENCY_RECOVERY)
+      descriptors.signers.signECPair({ psbt, ecpair: emergencyPair });
+    else descriptors.signers.signBIP32({ psbt, masterNode: unvaultMasterNode });
     //Finalize the tx (compute & add the scriptWitness) & push to the blockchain
-    descriptors.finalizePsbt({ psbt, descriptors: psbtInputDescriptors });
+    wshDescriptor.finalizePsbtInput({ index: 0, psbt });
     const spendTx = psbt.extractTransaction();
     const spendTxPushResult = await (
       await fetch(`${EXPLORER}/api/tx`, {
@@ -117,11 +133,14 @@ may need to register the Policy (only once) and then accept spending 2 utxos.`);
         body: spendTx.toHex()
       })
     ).text();
-    console.log({ pushedHex: spendTx.toHex() });
+    Log(`Pushing: ${spendTx.toHex()}`);
     Log(`Tx pushed with result: ${spendTxPushResult}`);
     //You may get non-bip68 final now. You need to wait 5 blocks.
-    if (spendTxPushResult.match('non-BIP68-final')) {
-      Log(`You still need to wait for a few more blocks (up to ${BLOCKS}).`);
+    if (
+      spendTxPushResult.match('non-BIP68-final') ||
+      spendTxPushResult.match('non-final')
+    ) {
+      Log(`Not final. You will need to wait up to ${BLOCKS} blocks.`);
       Log(`<a href="javascript:start();">Try again in a few blocks!</a>`);
     } else {
       const txId = spendTx.getId();
@@ -129,11 +148,9 @@ may need to register the Policy (only once) and then accept spending 2 utxos.`);
     }
   } else {
     Log(`Not yet! Use https://bitcoinfaucet.uo1.net to get some sats:`);
-    Log(`${wpkhAddress}: ${wpkhUtxo?.[0] ? 'Funded!' : 'NOT funded'}`);
-    Log(`${wshAddress}: ${wshUtxo?.[0] ? 'Funded!' : 'NOT funded'}`);
+    Log(`${wshAddress}: ${utxo?.[0] ? 'Funded!' : 'NOT funded'}`);
     Log(`Fund them and <a href="javascript:start();">check again</a>.`);
   }
-  */
 };
 if (isWeb) (window as any).start = start;
 
